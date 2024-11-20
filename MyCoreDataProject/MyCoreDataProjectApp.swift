5e8c139ddfd0d598c7f59271ce5977da3ecc9ce2
kahina lounis <kahinalounis2014@outlook.com>
2024-11-20 12:15:45 +0100

    Initial Commit

---
+++ MyCoreDataProject/MyCoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoreDataProjectApp.swift
+//  MyCoreDataProject
+//
+//  Created by Kahina Lounis on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyCoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
